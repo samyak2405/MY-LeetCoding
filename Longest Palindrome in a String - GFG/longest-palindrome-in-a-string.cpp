6b1b5751e0a40b29b3ff790d517572b9727c1b77
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-07 00:55:27 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
   public:
   
-    void solve(string s,int l,int r,int &max_len,int &st,int &end)
+    void solve(string s,int l,int r,int &max_len,int &st)
     {
         while(l>=0 and r<s.length())
         {
@@ -24,20 +24,19 @@ class Solution {
         {
             max_len = len;
             st = l+1;
-            end = r-1;
         }
     }
   
     string longestPalin (string s) {
         if(s.length()<2)
             return s;
-        int st = 0,end = 0;
+        int st = 0;
         int n = s.length();
         int max_len = 1;
         for(int i = 0;i<n-1;i++)
         {
-            solve(s,i,i,max_len,st,end);
-            solve(s,i,i+1,max_len,st,end);
+            solve(s,i,i,max_len,st);
+            solve(s,i,i+1,max_len,st);
         }
         
         return s.substr(st,max_len);
