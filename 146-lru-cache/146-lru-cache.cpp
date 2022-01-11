9070ac16108861174a82f666c0f02d4960567902
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-12 00:13:28 +0530

    Time: 809 ms (5.04%), Space: 178.7 MB (12.89%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -26,49 +26,37 @@ public:
     
     void addnode(node *newnode)
     {
-        node *temp = head->next;
-        newnode->next = temp;
-        newnode->prev = head;
+        newnode->next = head->next;
+        head->next->prev = newnode;
         head->next = newnode;
-        temp->prev = newnode;
+        newnode->prev = head;
+        mp[newnode->key] = newnode;
     }
     
     void deletenode(node *delnode)
     {
-        node *delprev = delnode->prev;
-        node *delnext = delnode->next;
-        delprev->next = delnext;
-        delnext->prev = delprev;
+        mp.erase(delnode->key);
+        delnode->next->prev = delnode->prev;
+        delnode->prev->next = delnode->next;
     }
     
     int get(int key) {
         if(mp.find(key)!=mp.end())
         {
-            node *resnode = mp[key];
-            int res = resnode->val;
-            mp.erase(key);
-            deletenode(resnode);
-            addnode(resnode);
-            mp[key] = head->next;
-            return res;
+            node *newnode = mp[key];
+            deletenode(newnode);
+            addnode(newnode);
+            return newnode->val;
         }
         return -1;
     }
     
     void put(int key, int value) {
         if(mp.find(key)!=mp.end())
-        {
-            node *existingnode = mp[key];
-            mp.erase(key);
-            deletenode(existingnode);
-        }
+            deletenode(mp[key]);
         if(mp.size()==cap)
-        {
-            mp.erase(tail->prev->key);
             deletenode(tail->prev);
-        }
         addnode(new node(key,value));
-        mp[key] = head->next;
     }
 };
 
