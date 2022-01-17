f854edd083ae6865a08f6aef84aef212a09badea
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-17 08:16:35 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (82.42%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,int> p2i;
+        unordered_map<string,int> w2i;
+        istringstream in(s);
+        int i = 0,n = pattern.length();
+        for(string word;in>>word;i++)
+        {
+            if(i==n or p2i[pattern[i]]!=w2i[word])
+                return false;
+            p2i[pattern[i]]=w2i[word]=i+1;
+        }
+        return i==n;
+    }
+};
