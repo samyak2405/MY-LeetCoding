574cb068b1a0a8818b77563707bcae9bab9dbc7e
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-20 08:08:18 +0530

    Time: 32 ms (98.47%), Space: 18.9 MB (53.71%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -3,27 +3,23 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int _max = 1000000000;
         int k = 1;
-        int ans = 0;
-        vector<int> v1 = piles;
-        while(k<=_max)
+        while(k<_max)
         {
             int mid = k + (_max-k)/2;
             int tot = 0;
-            for(auto it:v1)
+            for(auto it:piles)
             {
                 tot += (it+mid-1)/mid;
             }
             if(tot<=h)
             {
-                ans = mid;
-                _max = mid-1;
+                _max = mid;
             }
             else
             {
                 k = mid+1;
             }
-            v1 = piles;
         }
-        return ans;
+        return _max;
     }
 };
