450b71f42b97d7c80eec0f57290d29f5f8f6f3a1
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-20 07:52:06 +0530

    Time: 72 ms (27.11%), Space: 19.6 MB (12.47%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int k = 1;
         int _max = INT_MAX;
-        for(auto it:piles)
-            _max = max(_max,it);
+        int k = 1;
         int ans = 0;
-        vector<int> v1;
-        while(k<=_max){
-            int mid = k+(_max-k)/2;
-            int tot_h = 0;
-            for(auto it:v1){
+        vector<int> v1 = piles;
+        while(k<=_max)
+        {
+            int mid = k + (_max-k)/2;
+            int tot = 0;
+            for(auto it:v1)
+            {
                 if(mid>it)
-                    tot_h++;
-                else
-                {
-                    tot_h += it/mid;
+                    tot++;
+                else{
+                    tot+=it/mid;
                     if(it%mid!=0)
-                        tot_h++;
+                        tot++;
                 }
             }
-            if(tot_h<=h)
+            if(tot<=h)
             {
                 ans = mid;
                 _max = mid-1;
