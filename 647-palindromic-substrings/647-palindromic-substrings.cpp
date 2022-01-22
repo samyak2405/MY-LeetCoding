fa2ed691f63c9860b15304a8f14275e537cf7b11
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-22 15:25:39 +0530

    Time: 4 ms (87.84%), Space: 6.2 MB (83.30%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    
-    bool isPallindrome(string s,int i,int j)
-    {
-        while(i<j)
-        {
-            if(s[i]!=s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    
     int countSubstrings(string s) {
         int cnt = 0;
-        for(int i = 0;i<s.length();i++)
-        {
-            for(int j = i;j<s.length();j++)
-            {
-                if(isPallindrome(s,i,j))
-                    cnt++;
-            }
+        int n = s.length();
+        
+        for(int i =0;i<n;i++){
+            for(int l = i, r = i;(l>=0) and (r<n) and (s[l]==s[r]);l--,r++ )
+                cnt++;
+            for(int l = i,r = i+1;(l>=0) and (r<n) and (s[l]==s[r]);l--,r++)
+                cnt++;
         }
+        
         return cnt;
     }
 };
