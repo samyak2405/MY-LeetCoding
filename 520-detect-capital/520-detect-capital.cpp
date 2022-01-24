c442a32eb8932d59650299941372ced05f2390c1
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-24 09:15:02 +0530

    Time: 4 ms (27.21%), Space: 6.2 MB (19.00%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int up = 0,low =0;
+        for(int i = 0;i<word.size();i++)
+        {
+            if(word[i]>='A' and word[i]<='Z')
+                up++;
+            else
+                low++;
+        }
+        if((word[0]>='a' and word[0]<='z')and(up>=1) )
+            return false;
+        if(up>=1 and low==0)
+            return true;
+        if(up==1 and low!=0)
+            return true;
+        if(up==0 and low!=0)
+            return true;
+        return false;
+    }
+};
