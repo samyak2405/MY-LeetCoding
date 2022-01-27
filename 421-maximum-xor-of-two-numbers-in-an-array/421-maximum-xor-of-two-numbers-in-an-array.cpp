2ed9359fb38fcaf9251ecdbef1df1d726e3944ca
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-27 18:03:32 +0530

    Time: 365 ms (56.06%), Space: 65.3 MB (83.21%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,28 +1,81 @@
+struct Node{
+    Node *link[2] = {NULL};
+    
+    bool containsKey(int bit)
+    {
+        return link[bit]!=NULL;
+    }
+    
+    void put(int bit,Node *node)
+    {
+        link[bit] = node;
+    }
+    
+    Node *get(int bit)
+    {
+        return link[bit];
+    }
+};
+
+class Trie{
+  private:
+    Node *root;
+  public:
+    
+    Trie()
+    {
+        root = new Node();
+    }
+    
+    void insert(int num)
+    {
+        Node *node = root;
+        
+        for(int i = 31;i>=0;i--)
+        {
+            int bit = (num>>i)&1;
+            if(!node->containsKey(bit))
+                node->put(bit,new Node());
+            node = node->get(bit);
+        }
+    }
+    
+    int findMax(int num)
+    {
+        Node *node = root;
+        int maxNum = 0;
+        
+        for(int i = 31;i>=0;i--)
+        {
+            int bit = (num>>i)&1;
+            if(node->containsKey(1-bit))
+            {
+                maxNum |= (1<<i);
+                node = node->get(1-bit);
+            }
+            else
+                node = node->get(bit);
+        }
+        return maxNum;
+    }
+};
+
+
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int maxi = 0,mask = 0;
+        int maxi = 0;
+        int n = nums.size();
         
-        for(int i = 30;i>=0;i--)
-        {
-            set<int> s1;
-            
-            mask |= (1<<i);
-            
-            for(auto it:nums)
-                s1.insert(it&mask);
-            
-            int newmax = maxi |(1<<i);
-            
-            for(auto it:s1)
-            {
-                if(s1.count(it^newmax))
-                {
-                    maxi = newmax;
-                    break;
-                }
-            }
-        }
+        Trie *obj = new Trie();
+        
+        for(auto it:nums)
+            obj->insert(it);
+        
+        for(auto it:nums)
+            maxi = max(maxi,obj->findMax(it));
+        
         return maxi;
+        
     }
 };
