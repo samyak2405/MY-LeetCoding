1b33dedc0d55b516bc2b2dcd553be957f028a99f
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-01-27 17:15:09 +0530

    Added solution - LeetHub

--- Maximum XOR of two numbers in an array - GFG/maximum-xor-of-two-numbers-in-an-array.cpp
+++ Maximum XOR of two numbers in an array - GFG/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -7,86 +7,36 @@ using namespace std;
  // } Driver Code Ends
 //User function Template for C++
 
-struct Node{
-  Node *links[2]={NULL};
-  
-  bool containsKey(int bit)
-  {
-      return links[bit]!=NULL;
-  }
-  
-  void put(int bit,Node *node)
-  {
-      links[bit] = node;
-  }
-  
-  Node *get(int bit)
-  {
-      return links[bit];
-  }
-};
-
-class Trie{
-    private:
-        Node *root;
-    public:
-    Trie()
-    {
-        root = new Node();
-    }
-    
-    void insert(int num)
-    {
-        Node *node = root;
-        
-        for(int i =31;i>=0;i--)
-        {
-            int bit = (num>>i)&1;
-            if(!node->containsKey(bit))
-                node->put(bit,new Node());
-            node=node->get(bit);
-        }
-    }
-    
-    int findMax(int num)
-    {
-        Node *node = root;
-        int maxNum = 0;
-        
-        for(int i = 31;i>=0;i--)
-        {
-            int bit = (num>>i)&1;
-            if(node->containsKey(1-bit))
-            {
-                maxNum |= (1<<i);
-                node = node->get(1-bit);
-            }
-            else
-                node = node->get(bit);
-        }
-        return maxNum;
-    }
-};
-
-
 class Solution
 {
     public:
     int max_xor(int arr[] , int n)
     {
         int maxi = 0;
+        int mask = 0;
         
-        Trie *obj = new Trie();
-        
-        for(int i = 0;i<n;i++)
-            obj->insert(arr[i]);
+        for(int i = 30;i>=0;i--)
+        {
+            set<int> s1;
             
-        for(int i = 0;i<n;i++)
-            maxi = max(maxi,obj->findMax(arr[i]));
+            mask |= (1<<i);
             
-        return maxi; 
+            for(int j = 0;j<n;j++)
+                s1.insert(mask&arr[j]);
+                
+            int newmax = maxi|(1<<i);
+            
+            for(int prefix:s1)
+            {
+                if(s1.count(newmax^prefix))
+                {
+                    maxi = newmax;
+                    break;
+                }
+            }
+        }
+        return maxi;
     }
-    
 };
 
 
