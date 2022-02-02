fb336f8fb8cca9bd7052fcba7efa87ae0c6c109c
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-02 15:37:09 +0530

    Time: 4 ms (70.52%), Space: 8.3 MB (33.25%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        set<vector<int>> res1;
+        for(int i = 0;i<pow(2,n);i++)
+        {
+            vector<int> tmp;
+            for(int j = 0;j<n;j++)
+                if(i&(1<<j))
+                    tmp.push_back(nums[j]);
+            sort(tmp.begin(),tmp.end());
+            res1.insert(tmp);
+        }
+        for(auto it:res1)
+            res.push_back(it);
+        return res;
+    }
+};
