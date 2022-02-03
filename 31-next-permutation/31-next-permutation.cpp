4a1dd415c43bcf0c86e973026cfa01a152ee98de
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-03 10:49:32 +0530

    Time: 6 ms (49.20%), Space: 12.1 MB (36.44%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,7 +1,37 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        return ;
+        int n = nums.size();
+        int i = n-1;
+        while(i-1>=0)
+        {
+            if(nums[i]>nums[i-1])
+                break;
+            i--;
+        }
+        // cout<<i<<endl;
+        if(i==0)
+        {
+            sort(nums.begin(),nums.end());
+            return ;
+        }
+        int a = nums[i-1];
+        int j = i;
+        for(;j<n;j++)
+        {
+            if(nums[j]<=a)
+                break;
+        }
+        swap(nums[i-1],nums[j-1]);
+        // for(auto it:nums)
+        //     cout<<it<<" ";
+        // cout<<endl;
+        j = n-1;
+        while(i<j)
+        {
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
     }
 };
