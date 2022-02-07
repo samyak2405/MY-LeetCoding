9e5cce9d10cca91b3a3c8e3a6c8e674833525f32
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-08 01:48:16 +0530

    Added solution - LeetHub

--- Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.cpp
+++ Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.cpp
@@ -9,32 +9,18 @@ using namespace std;
 
 class Solution {
   public:
-  
-    bool isPrime(int n)
-    {
-        if(n==1)
-            return false;
-        if(n==2 or n==3)
-            return true;
-        int cnt = 0;
-        
-        for(int i = 2;i*i<=n;i++)
-        {
-            if(n%i==0)
-                return false;
-        }
-        return true;
-    }
-  
     vector<int> primeRange(int M, int N) {
-        vector<int> res;
-        
+        vector<int> seive(N+1,false);
+        for(int i = 2;i<N+1;i++)
+            seive[i] = true;
+        for(int i = 2;i*i<N+1;i++)
+            if(seive[i])
+                for(int j = i*i;j<N+1;j+=i)
+                    seive[j] = false;
+        vector<int> res; 
         for(int i = M;i<=N;i++)
-        {
-            if(isPrime(i))
+            if(seive[i])
                 res.push_back(i);
-        }
-        
         return res;
     }
 };
