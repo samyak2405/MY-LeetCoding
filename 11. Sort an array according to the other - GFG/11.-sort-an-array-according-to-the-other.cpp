dbb972ee8b17d491ccd1a0d9f4e9b16bec01ce54
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-08 00:44:06 +0530

    Added solution - LeetHub

---
+++ 11. Sort an array according to the other - GFG/11.-sort-an-array-according-to-the-other.cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template in C++
+
+
+class Solution{
+    public:
+    // A1[] : the input array-1
+    // N : size of the array A1[]
+    // A2[] : the input array-2
+    // M : size of the array A2[]
+    
+    //Function to sort an array according to the other array.
+    vector<int> sortA1ByA2(vector<int> arr1, int n, vector<int> arr2, int m) 
+    {
+        vector<int> res;
+        map<int,int> mp;
+        for(auto it:arr1)
+        {
+            mp[it]++;
+        }
+        
+        for(auto it:arr2)
+        {
+            if(mp.find(it)!=mp.end())
+            {
+                int cnt = mp[it];
+                while(cnt--)
+                {
+                    res.push_back(it);
+                }
+                mp.erase(it);
+            }
+        }
+        for(auto it:mp)
+            while(it.second--)
+                res.push_back(it.first);
+        return res;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main(int argc, char *argv[]) 
+{ 
+	
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    vector<int> a1(n);
+	    vector<int> a2(m);
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> a1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> a2[i];
+	    }
+	    
+	    Solution ob;
+	    a1 = ob.sortA1ByA2(a1, n, a2, m); 
+	
+	   
+	    for (int i = 0; i < n; i++) 
+		    cout<<a1[i]<<" ";
+		
+	    cout << endl;
+	    
+	    
+	}
+	return 0; 
+} 
+  // } Driver Code Ends
