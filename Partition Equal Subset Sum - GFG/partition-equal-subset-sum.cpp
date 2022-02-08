a770f0ac3f41ea72360719f92062cbe74f7c4d26
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-09 00:33:38 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -10,29 +10,23 @@ using namespace std;
 class Solution{
 public:
 
-    bool isSubset(int arr[],int N,int sum)
+    bool subset_sum(int arr[],int n,int sum)
     {
-        int dp[N+1][sum+1];
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
         
-        for(int i =0;i<N+1;i++)
-            dp[i][0] = true;
-        
-        for(int i = 0;i<sum+1;i++)
-            dp[0][i] = false;
-        
-        for(int i = 1;i<N+1;i++)
+        for(int i = 0;i<n+1;i++)
+            dp[i][0] = 1;
+        for(int i = 1;i<n+1;i++)
+        {
             for(int j = 1;j<sum+1;j++)
             {
                 if(arr[i-1]<=j)
-                {
                     dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
-                }
-                else{
+                else
                     dp[i][j] = dp[i-1][j];
-                }
             }
-            
-        return dp[N][sum];
+        }
+        return dp[n][sum];
     }
 
     int equalPartition(int N, int arr[])
@@ -43,7 +37,9 @@ public:
             
         if(sum%2!=0)
             return false;
-        return isSubset(arr,N,sum/2);
+        sum/=2;
+        
+        return subset_sum(arr,N,sum);
     }
 };
 
