26ee1eb1b3544cd23d1e297a4e869590bd484ed2
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-13 08:33:06 +0530

    Time: 7 ms (26.93%), Space: 7.2 MB (53.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,14 +3,18 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         int n = nums.size();
+        
         for(int i = 0;i<pow(2,n);i++)
         {
             vector<int> tmp;
             for(int j = 0;j<n;j++)
-                if(i&(1<<j))
+            {
+                if(1&(i>>j))
                     tmp.push_back(nums[j]);
+            }
             res.push_back(tmp);
         }
+        
         
         return res;
     }
