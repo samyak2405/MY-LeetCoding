dd4fa0e5d6b61743f368c25270be2ef0283e29a0
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-14 15:39:27 +0530

    Time: 71 ms (65.23%), Space: 33.6 MB (94.17%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size();
-        int arr[n+1] ;
-        memset(arr,0,sizeof(arr));
-        for(int i = 0;i<n;i++)
-            arr[nums[i]]++;
-        vector<int> ans;
-        for(int i = 1;i<=n;i++)
-            if(arr[i]==0)
-                ans.push_back(i);
-        return ans;
+        vector<int> res;
+        for(int i = 0;i<nums.size();i++)
+        {
+            int m = abs(nums[i])-1;
+            nums[m] = nums[m]>0?-nums[m]:nums[m];
+        }
+        for(int i = 0;i<nums.size();i++)
+            if(nums[i]>0)
+                res.push_back(i+1);
+        
+        return res;
     }
 };
