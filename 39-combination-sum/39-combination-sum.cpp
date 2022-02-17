543c1672760ca684440e93158ff250cf4ea6e722
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-17 07:28:45 +0530

    Time: 4 ms (93.36%), Space: 10.9 MB (72.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
+    typedef vector<vector<int>> vvi;
+    typedef vector<int> vi;
     
-    void combine(vector<int> &nums,int target,vector<vector<int>> &res,int index,vector<int> tmp)
+    void combine(vi &candidates,int target,int index,vvi &res,vi &tmp)
     {
         if(target<0)
             return;
-        if(index==nums.size())
+        if(index==candidates.size())
         {
             if(!target)
                 res.push_back(tmp);
             return;
         }
-        if(nums[index]<=target)
+        if(candidates[index]<=target)
         {
-            tmp.push_back(nums[index]);
-            combine(nums,target-nums[index],res,index,tmp);
+            tmp.push_back(candidates[index]);
+            combine(candidates,target-candidates[index],index,res,tmp);
             tmp.pop_back();
         }
-        combine(nums,target,res,index+1,tmp);
-        
+        combine(candidates,target,index+1,res,tmp);
     }
     
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> res;
-        vector<int> tmp;
-        combine(nums,target,res,0,tmp);
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vvi res;
+        vi tmp;
+        combine(candidates,target,0,res,tmp);
         return res;
-        
     }
 };
