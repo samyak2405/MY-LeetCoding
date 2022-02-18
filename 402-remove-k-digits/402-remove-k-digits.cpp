ef32b31a82c594239438549952ed8a20de740b24
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-02-18 10:26:22 +0530

    Time: 3 ms (91.01%), Space: 6.8 MB (95.64%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,48 +1,60 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        if(k==num.length())
-            return "0";
-        if(k==0)
-            return num;
-        stack<char> s1;
-        int i = 0;
-        while(i<num.length() and k)
+        string ans;
+        for(auto it:num)
         {
-            if(!s1.empty() and s1.top()>num[i])
-            {
-                while(!s1.empty() and s1.top()>num[i] and k)
-                {
-                    s1.pop();
-                    k--;
-                }
-                if(k==0)
-                    break;
-            }
-            s1.push(num[i]);
-            i++;
+            while(ans.length() and ans.back()>it and k)
+                ans.pop_back(),k--;
+            if(ans.length() or it!='0')
+                ans.push_back(it);
         }
-        while(!s1.empty() and k)
-        {
-            s1.pop();
-            k--;
-        }
-        string res;
-        while(!s1.empty())
-        {
-            res+=s1.top();
-            s1.pop();
-        }
-        reverse(res.begin(),res.end());
-        while(i<num.length())
-            res+=num[i++];
-        string res1;
-        i = 0;
-        while(res[i]=='0')
-            i++;
-        res1.append(res.substr(i,res.length()));
-        if(!res1.size())
-            return "0";
-        return res1;
+        while(ans.length() and k--)
+            ans.pop_back();
+        
+        return ans.empty()?"0":ans;
+        // if(k==num.length())
+        //     return "0";
+        // if(k==0)
+        //     return num;
+        // stack<char> s1;
+        // int i = 0;
+        // while(i<num.length() and k)
+        // {
+        //     if(!s1.empty() and s1.top()>num[i])
+        //     {
+        //         while(!s1.empty() and s1.top()>num[i] and k)
+        //         {
+        //             s1.pop();
+        //             k--;
+        //         }
+        //         if(k==0)
+        //             break;
+        //     }
+        //     s1.push(num[i]);
+        //     i++;
+        // }
+        // while(!s1.empty() and k)
+        // {
+        //     s1.pop();
+        //     k--;
+        // }
+        // string res;
+        // while(!s1.empty())
+        // {
+        //     res+=s1.top();
+        //     s1.pop();
+        // }
+        // reverse(res.begin(),res.end());
+        // while(i<num.length())
+        //     res+=num[i++];
+        // string res1;
+        // i = 0;
+        // while(res[i]=='0')
+        //     i++;
+        // res1.append(res.substr(i,res.length()));
+        // if(!res1.size())
+        //     return "0";
+        // return res1;
     }
 };
