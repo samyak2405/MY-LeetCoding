a3f56e5d99f1853ce7b9196f6b3b93d94cb7e6a2
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-03-10 16:46:11 +0530

    Added solution - LeetHub

---
+++ 6. Pairwise swap elements of a linked list - GFG/6.-pairwise-swap-elements-of-a-linked-list.cpp
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+class Solution
+{
+    public:
+    Node* pairWiseSwap(struct Node* head) 
+    {
+        if(!head or !head->next)
+            return head;
+        Node *dum = new Node(0);
+        dum->next = head;
+        Node *prev = dum,*p = head,*q = head->next;
+        while(p and q)
+        {
+            p->next = q->next;
+            q->next = p;
+            prev->next = q;
+            prev = p;
+            p = p->next?p->next:NULL;
+            q = p?p->next:NULL;
+        }
+        return dum->next;
+    }
+};
+
+// { Driver Code Starts.
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
+}
+  // } Driver Code Ends
