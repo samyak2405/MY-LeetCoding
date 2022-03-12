327988809d32527d7db92035e79f26d10a6908ab
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-03-12 08:04:20 +0530

    Added solution - LeetHub

---
+++ 20. Intersection of two sorted Linked lists - GFG/20.-intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    if(!head1 and !head2)
+        return NULL;
+    if(!head1)
+        return head2;
+    if(!head2)
+        return head1;
+    Node *p = head1,*q = head2;
+    Node *head = new Node(0);
+    Node *tail = head;
+    while(p and q)
+    {
+        if(p->data>q->data)
+            q = q->next;
+        else if(p->data<q->data)
+            p = p->next;
+        else
+        {
+            tail->next = new Node(p->data);
+            tail = tail->next;
+            p = p->next;
+            q = q->next;
+        }
+    }
+    return head->next;
+}
