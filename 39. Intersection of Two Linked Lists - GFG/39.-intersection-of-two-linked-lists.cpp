eac1f10c0be4eb81e11aa2ed31df28918ee3f17c
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-03-14 16:18:54 +0530

    Added solution - LeetHub

---
+++ 39. Intersection of Two Linked Lists - GFG/39.-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,113 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+ // } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        unordered_set<int> s1;
+        Node *p = head2;
+        while(p)
+        {
+            s1.insert(p->data);
+            p = p->next;
+        }
+        Node *head = new Node(0);
+        Node *tail = head;
+        p = head1;
+        while(p)
+        {
+            if(s1.find(p->data)!=s1.end())
+            {
+                Node *newnode = new Node(p->data);
+                tail->next = newnode;
+                tail = newnode;
+            }
+            p = p->next;
+        }
+        return head->next;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
