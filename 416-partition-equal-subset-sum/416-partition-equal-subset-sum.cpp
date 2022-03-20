77efa5ff34d3c9f9321325f31c995c469af8b0d6
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-03-20 22:50:17 +0530

    Time: 559 ms (11.57%), Space: 12 MB (57.24%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    
-    bool isSubset(vector<int> &arr,int N,int sum)
-    {
-        int dp[N+1][sum+1];
-        
-        for(int i =0;i<N+1;i++)
-            dp[i][0] = true;
-        
-        for(int i = 0;i<sum+1;i++)
-            dp[0][i] = false;
-        
-        for(int i = 1;i<N+1;i++)
-            for(int j = 1;j<sum+1;j++)
-            {
-                if(arr[i-1]<=j)
-                {
-                    dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-            
-        return dp[N][sum];
-    }
-    
-    bool canPartition(vector<int>& arr) {
+    bool canPartition(vector<int>& nums) {
         int sum = 0;
-        int N = arr.size();
-        for(int i = 0;i<N;i++)
-            sum+=arr[i];
-            
-        if(sum%2!=0)
+        int n = nums.size();
+        for(int i = 0;i<n;i++)
+            sum+=nums[i];
+        if(sum%2)
             return false;
-        return isSubset(arr,N,sum/2);
+        sum/=2;
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,0));
+        for(int i = 0;i<=n;i++)
+        {
+            for(int j = 0;j<=sum;j++)
+            {
+                if(i==0)
+                    dp[i][j] = 0;
+                if(j==0)
+                    dp[i][j] = 1;
+                if(i>0 and nums[i-1]<=j)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] or dp[i-1][j];
+                else if(i>0 and nums[i-1]>j)
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
     }
 };
