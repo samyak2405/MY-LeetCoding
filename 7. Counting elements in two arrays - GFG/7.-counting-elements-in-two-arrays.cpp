0e10b9d36662dfcc98d7331d89ecdf1d35b31efc
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-09 22:29:13 +0530

    Added solution - LeetHub

--- 7. Counting elements in two arrays - GFG/7.-counting-elements-in-two-arrays.cpp
+++ 7. Counting elements in two arrays - GFG/7.-counting-elements-in-two-arrays.cpp
@@ -21,14 +21,9 @@ class Solution{
         {
             int mid = low+(high-low)/2;
             if(arr[mid]>x)
-            {
                 high = mid-1;
-            }
             else
-            {
-               
                 low = mid+1;
-            }
         }
         
         return low;
