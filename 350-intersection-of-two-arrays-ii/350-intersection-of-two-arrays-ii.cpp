2fdc8cbcd1c1d4562b8ac15ee47bd577ca80cc38
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-11 16:14:02 +0530

    Time: 10 ms (36.74%), Space: 10.7 MB (31.51%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp;
+        for(auto it:nums1)
+            mp[it]++;
+        vector<int> res;
+        for(auto it:nums2)
+            if(mp[it]>0)
+            {
+                res.push_back(it);
+                mp[it]--;
+            }
+        return res;
+    }
+};
