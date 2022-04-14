e107f3cea8c5689f56d99b9f495667bf29df38b4
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-14 07:17:51 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (56.83%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0)
-            return 0;
-        if(n==1)
-            return nums[0];
-        int maxim = 0;
+        int m = 0;
         for(int i :nums)
-            maxim = max(maxim,i);
-        int dp[maxim+1];
+            m = max(m,i);
+        int dp[m+1];
         memset(dp,0,sizeof(dp));
         for(int i = 0;i<nums.size();i++)
-            dp[nums[i]]++;
-        int inc = 0;
-        int exc = 0;
-        for(int i = 0;i<maxim+1;i++)
+            dp[nums[i]]+=nums[i];
+        int take = 0,skip = 0;
+        for(int i = 0;i<m+1;i++)
         {
-            int new_inc = exc + dp[i]*i;
-            int new_exc = max(inc,exc);
-            
-            inc = new_inc;
-            exc = new_exc;
+            int takei = dp[i] + skip;
+            int skipi = max(skip,take);
+            take = takei;
+            skip = skipi;
         }
-        return max(inc,exc);
+        return max(take,skip);
     }
 };
