7fa7fd39a6cb6e5d5baa694d6070eaa585fe0589
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-20 11:39:56 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    int n = str1.length(),m = str2.length();
+	    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+	    
+	    for(int i = 0;i<n+1;i++)
+	    {
+	        for(int j = 0;j<m+1;j++)
+	        {
+	            if(i==0 or j==0)
+	                dp[i][j] = 0;
+	            else if(str1[i-1]==str2[j-1])
+	                dp[i][j] = 1+dp[i-1][j-1];
+	            else
+	                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+	        }
+	    }
+	    int cnt = n - dp[n][m];
+	    cnt += m - dp[n][m];  
+	    return cnt;
+	} 
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
