cf93d189e8726b43a588638c9212380282126d35
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-21 09:46:15 +0530

    Time: 4 ms (76.19%), Space: 11.9 MB (24.99%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -20,15 +20,8 @@ public:
         vector<vector<int>> dp(n+2,vector<int>(2,0));
         for(int i = n-1;i>=0;i--)
         {
-            int profit = 0;
-            for(int j = 0;j<2;j++)
-            {
-                if(j)
-                    profit = max(-prices[i]+dp[i+1][0],dp[i+1][1]);
-                else
-                    profit = max(prices[i]+dp[i+2][1],dp[i+1][0]);
-                dp[i][j] = profit;
-            }
+            dp[i][1]= max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0]= max(prices[i]+dp[i+2][1],dp[i+1][0]);
         }
         return dp[0][1];
     }
