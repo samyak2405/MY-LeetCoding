92018fa818ff47a1dd820c18668d1417feacee96
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-21 10:13:11 +0530

    Added solution - LeetHub

---
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxProfit(int k, int n, int prices[]) {
+        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        
+        // for(int i = n-1;i>=0;i--)
+        // {
+        //     int profit = 0;
+        //     for(int j = 0;j<2;j++)
+        //     {
+        //         for(int l = 1;l<3;l++)
+        //         {
+        //             if(j)
+        //                 profit = max(-prices[i] + dp[i+1][0][l],dp[i+1][1][l]);
+        //             else
+        //                 profit = max(prices[i] + dp[i+1][1][l-1],dp[i+1][0][l]);
+        //             dp[i][j][l] = profit;
+        //         }
+        //     }
+        // }
+        // return dp[0][1][k];
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int>(k+1,0)));
+        
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = 0;j<2;j++)
+            {
+                int profit = 0;
+                for(int l = 1;l<k+1;l++)
+                {
+                    if(j)
+                        profit = max(-prices[i]+dp[i+1][0][l],dp[i+1][1][l]);
+                    else
+                        profit = max(prices[i]+dp[i+1][1][l-1],dp[i+1][0][l]);
+                    dp[i][j][l] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> K;
+        cin >> N;
+        int A[N];
+        for (int i = 0; i < N; i++) cin >> A[i];
+
+        Solution ob;
+        cout << ob.maxProfit(K, N, A) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
