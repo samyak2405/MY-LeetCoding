ea9718ee0e53ed789b7389a33496da4526fdb552
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-21 09:37:04 +0530

    Time: 3 ms (86.85%), Space: 12 MB (19.39%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,7 +17,7 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        vector<vector<int>> dp(n,vector<int>(2,-1));
         return profit(prices,0,1,dp);
     }
 };
