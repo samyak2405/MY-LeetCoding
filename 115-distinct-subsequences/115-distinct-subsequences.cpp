c804cd132bbce38c60f6422e50f8334a789d63dc
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-22 10:06:47 +0530

    Time: 2757 ms (5.06%), Space: 422.7 MB (5.01%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     
-    // int solve(string s,string t,int i,int j,vector<vector<int>> &dp)
-    // {
-    //     if(j<0)
-    //         return 1;
-    //     if(i<0)
-    //         return 0;
-    //     if(dp[i][j]!=-1)
-    //         return dp[i][j];
-    //     if(s[i]==t[j])
-    //         return dp[i][j] = solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
-    //     else
-    //         return dp[i][j] = solve(s,t,i-1,j,dp);
-    // }
+    int solve(string s,string t,int i,int j,vector<vector<int>> &dp)
+    {
+        if(j==0)
+            return 1;
+        if(i==0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(s[i-1]==t[j-1])
+            return dp[i][j] = solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
+        else
+            return dp[i][j] = solve(s,t,i-1,j,dp);
+    }
     
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        // vector<vector<int>> dp(n,vector<int>(m,-1));
-        // return solve(s,t,n-1,m-1,dp);
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(s,t,n,m,dp);
+//         vector<vector<double>> dp(n+1,vector<double>(m+1,0));
         
-        for(int i = 0;i<n+1;i++)
-            dp[i][0] = 1;
+//         for(int i = 0;i<n+1;i++)
+//             dp[i][0] = 1;
         
-        for(int i = 1;i<n+1;i++)
-        {
-            for(int j = 1;j<m+1;j++)
-            {
-                if(s[i-1]==t[j-1])
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
+//         for(int i = 1;i<n+1;i++)
+//         {
+//             for(int j = 1;j<m+1;j++)
+//             {
+//                 if(s[i-1]==t[j-1])
+//                     dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+//                 else
+//                     dp[i][j] = dp[i-1][j];
+//             }
+//         }
         
-        return (int)dp[n][m];
+//         return (int)dp[n][m];
         
     }
 };
