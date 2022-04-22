fe1e4d157ba253c7426f19d5d853284dfa719431
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-22 10:15:54 +0530

    Time: 2922 ms (5.06%), Space: 422.7 MB (5.01%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -37,6 +37,21 @@ public:
 //         }
         
 //         return (int)dp[n][m];
+        vector<double> prev(m+1,0),curr(m+1,0);
+        prev[0] = curr[0] = 1;
+        
+        for(int i = 1;i<n+1;i++)
+        {
+            for(int j = 1;j<m+1;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    curr[j] = prev[j-1]+prev[j];
+                else
+                    curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[m];
         
     }
 };
