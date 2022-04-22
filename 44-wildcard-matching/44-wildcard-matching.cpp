cd80fcb84f89229908f13586dde5febf9ee0ae78
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-23 00:15:44 +0530

    Time: 73 ms (53.40%), Space: 27.1 MB (28.94%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,32 +1,9 @@
 class Solution {
 public:
     
-    // bool f(string p,string s,int i,int j,vector<vector<int>> &dp)
-    // {
-    //     if(i==0 and j==0)
-    //         return true;
-    //     if(i==0 and j>0)
-    //         return false;
-    //     if(j==0 and i>0)
-    //     {
-    //         for(int k = 1;k<=i;k++)
-    //             if(p[k-1]!='*')
-    //                 return false;
-    //         return true;
-    //     }
-    //     if(dp[i][j]!=-1)
-    //         return dp[i][j];
-    //     if(p[i-1]==s[j-1] or p[i-1]=='?')
-    //         return dp[i][j] = f(p,s,i-1,j-1,dp);
-    //     if(p[i-1]=='*')
-    //         return dp[i][j] = f(p,s,i-1,j,dp) or f(p,s,i,j-1,dp);
-    //     return false;
-    // }
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        // vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        // return f(p,s,m,n,dp);
         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         dp[0][0] = 1;
         for(int i = 1;i<n+1;i++)
