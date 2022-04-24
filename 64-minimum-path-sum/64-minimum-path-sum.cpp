8110b2bae9015a89ee922b7e2f224bdea9823761
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-24 13:04:39 +0530

    Time: 13 ms (47.17%), Space: 10 MB (57.72%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -17,28 +17,51 @@ public:
         int n = grid[0].size();
         // vector<vector<int>> dp(m,vector<int>(n,-1));
         // return f(m-1,n-1,grid,dp);
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         
+//         for(int i = 0;i<m+1;i++)
+//         {
+//             for(int j = 0;j<n+1;j++)
+//             {
+//                 if(i==0 or j==0)
+//                     dp[i][j] = INT_MAX;
+//                 else if(i==1 and j==1)
+//                     dp[i][j] = grid[i-1][j-1];
+//                 else
+//                 {
+//                     int up = 0,left = 0;
+//                     if(i>0 and j>=0)
+//                         up = dp[i-1][j];
+//                     if(j>0 and i>=0)
+//                         left = dp[i][j-1];
+//                     dp[i][j] = grid[i-1][j-1]+min(up,left);
+//                 }
+//             }
+//         }
+        
+//         return dp[m][n];
+        vector<int>prev(n+1,0);
         for(int i = 0;i<m+1;i++)
         {
+            vector<int> curr(n+1,0);
             for(int j = 0;j<n+1;j++)
             {
                 if(i==0 or j==0)
-                    dp[i][j] = INT_MAX;
+                    prev[j] = curr[j] = INT_MAX;
                 else if(i==1 and j==1)
-                    dp[i][j] = grid[i-1][j-1];
+                    curr[j] = grid[i-1][j-1];
                 else
                 {
-                    int up = 0,left = 0;
+                    int left = 0,up = 0;
                     if(i>0 and j>=0)
-                        up = dp[i-1][j];
+                        up = prev[j];
                     if(j>0 and i>=0)
-                        left = dp[i][j-1];
-                    dp[i][j] = grid[i-1][j-1]+min(up,left);
+                        left = curr[j-1];
+                    curr[j] = grid[i-1][j-1]+min(up,left);
                 }
             }
+            prev = curr;
         }
-        
-        return dp[m][n];
+        return prev[n];
     }
 };
