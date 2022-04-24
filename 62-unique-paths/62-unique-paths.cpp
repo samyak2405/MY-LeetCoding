1d60cfbc5cde0c61b4b70f7224e12c2994cf7344
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-24 11:53:15 +0530

    Time: 2 ms (40.57%), Space: 6.1 MB (57.79%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -17,24 +17,45 @@ public:
     int uniquePaths(int m, int n) {
         // vector<vector<int>> dp(m,vector<int>(n,-1));
         // return f(m-1,n-1,dp);
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        // vector<vector<int>> dp(m,vector<int>(n,0));
+        // for(int i = 0;i<m;i++)
+        // {
+        //     for(int j = 0;j<n;j++)
+        //     {
+        //         if(i==0 and j==0)
+        //             dp[i][j] = 1;
+        //         else
+        //         {
+        //             int up = 0,left = 0;
+        //             if(i>0 and j>=0)
+        //                 up = dp[i-1][j];
+        //             if(j>0 and i>=0)
+        //                 left = dp[i][j-1]; 
+        //             dp[i][j] = up+left;
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+        vector<int> prev(n,0);
         for(int i = 0;i<m;i++)
         {
+            vector<int> curr(n,0);
             for(int j = 0;j<n;j++)
             {
                 if(i==0 and j==0)
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 else
                 {
-                    int up = 0,left = 0;
+                    int up=0,left = 0;
                     if(i>0 and j>=0)
-                        up = dp[i-1][j];
+                        up = prev[j];
                     if(j>0 and i>=0)
-                        left = dp[i][j-1]; 
-                    dp[i][j] = up+left;
+                        left = curr[j-1];
+                    curr[j] = up+left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
