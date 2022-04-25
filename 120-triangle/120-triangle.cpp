0dc1e3415bf0a161ef0b49acd62e27808d716a74
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-26 00:25:00 +0530

    Time: 9 ms (44.90%), Space: 9.1 MB (23.44%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -17,16 +17,12 @@ public:
         int m = triangle[n-1].size();
         // vector<vector<int>> dp(n,vector<int>(n,-1));
         // return f(0,0,n,triangle,dp);
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i = 0;i<m;i++)
-            dp[m-1][i] = triangle[m-1][i];
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i = 0;i<n;i++)
+            dp[n-1][i] = triangle[n-1][i];
         for(int i = n-2;i>=0;i--)
-        {
             for(int j = i;j>=0;j--)
-            {
                 dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
-            }
-        }
         return dp[0][0];
     }
 };
