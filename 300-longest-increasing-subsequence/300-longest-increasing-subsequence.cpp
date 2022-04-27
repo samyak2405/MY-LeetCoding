a45dd9c9d7260b290ddd66b28fe4b5f2feb16175
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-27 05:30:43 +0530

    Time: 461 ms (20.77%), Space: 10.6 MB (41.27%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    
-//     int f(int ind,int prev,vector<int> &nums,vector<vector<int>> &dp)
-//     {
-//         if(ind==nums.size())
-//             return 0;
-//         if(dp[ind][prev+1]!=-1)
-//             return dp[ind][prev+1];
-//         int len = f(ind+1,prev,nums,dp);
-//         if(prev==-1 or nums[prev]<nums[ind])
-//             len = max(len,1+f(ind+1,ind,nums,dp));
-//         return dp[ind][prev+1] = len;
-//     }
-    
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        // return f(0,-1,nums,dp);
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        vector<int> curr(n+1,0),ahead(n+1,0);
-        for(int ind = n-1;ind>=0;ind--)
+        vector<int> dp(nums.size(),1);
+        int maxi = INT_MIN;
+        for(int i = 0;i<nums.size();i++)
         {
-            for(int prev = ind-1;prev>=-1;prev--)
+            for(int j = 0;j<i;j++)
             {
-                int len = ahead[prev+1];
-                if(prev == -1 or nums[prev]<nums[ind])
-                    len = max(len,1+ahead[ind+1]);
-                curr[prev+1] = len;
+                if(nums[j]<nums[i])
+                    dp[i] = max(dp[i],1+dp[j]);
             }
-            ahead = curr;
+            maxi = max(maxi,dp[i]);
         }
-        return ahead[0];
+        return maxi;
     }
 };
