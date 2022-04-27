5f211e9a952e4e6e014fb9acff2053e7dc7fb9a5
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-04-27 06:11:27 +0530

    Added solution - LeetHub

---
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+		
+
+	public:
+	
+	int f(int ind,int prev,int arr[],int n)
+	{
+	}
+	
+	int maxSumIS(int arr[], int n)  
+	{  
+	    vector<int> dp(n,1);
+	    int maxi = 0;
+	    for(int i = 0;i<n;i++)
+	    {
+	        dp[i] = arr[i];
+	        for(int j = 0;j<i;j++)
+	        {
+	            if(arr[j]<arr[i])
+	                dp[i] = max(dp[i],arr[i]+dp[j]);
+	        }
+	        maxi = max(maxi,dp[i]);
+	    }
+	    
+	    return maxi;
+	}  
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
