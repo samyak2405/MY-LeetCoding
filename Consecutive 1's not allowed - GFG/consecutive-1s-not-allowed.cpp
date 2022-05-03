4b5c3ce1e174745248a1b4ad5453d8872e16bb68
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-03 13:21:02 +0530

    Added solution - LeetHub

---
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	
+	ll count(int n,vector<ll> &dp,int mod)
+	{
+	    if(n==1)
+	        return dp[n] = 2;
+	    if(n==2)
+	        return dp[n] = 3;
+	    if(dp[n]!=-1)
+	        return dp[n];
+	    
+	    return dp[n] = (count(n-1,dp,mod)%mod+count(n-2,dp,mod)%mod)%mod;
+	}
+	
+	ll countStrings(int n) {
+	    vector<ll> dp(n+1,-1);
+	    return count(n,dp,1e9+7);
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
