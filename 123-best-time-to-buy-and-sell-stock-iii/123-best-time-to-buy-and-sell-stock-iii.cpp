b8a15226cb0f38aa7c61869227b75fac1d0ec359
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-03 18:23:58 +0530

    Time: 1420 ms (5.03%), Space: 282.2 MB (5.08%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     
+    // int f(int ind,bool buy,vector<int> &prices,int cap)
+    // {
+    //     if(ind==prices.size())
+    //         return 0;
+    //     if(!cap)
+    //         return 0;
+    //     int profit = 0;
+    //     if(buy)
+    //         profit = max(-prices[ind]+f(ind+1,0,prices,cap),f(ind+1,1,prices,cap));
+    //     else
+    //         profit = max(prices[ind]+f(ind+1,1,prices,cap-1),f(ind+1,0,prices,cap));
+    //     return profit;
+    // }
+    int f(int ind,bool buy,vector<int> &prices,int cap,vector<vector<vector<int>>> &dp)
+    {
+        if(ind==prices.size())
+            return 0;
+        if(!cap)
+            return 0;
+        if(dp[ind][buy][cap]!=-1)
+            return dp[ind][buy][cap];
+        int profit = 0;
+        if(buy)
+            profit = max(-prices[ind]+f(ind+1,0,prices,cap,dp),f(ind+1,1,prices,cap,dp));
+        else
+            profit = max(prices[ind]+f(ind+1,1,prices,cap-1,dp),f(ind+1,0,prices,cap,dp));
+        return dp[ind][buy][cap] = profit;
+    }
+    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> after(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
+        //recursion
+        // return f(0,1,prices,2);
         
-        for(int i = n-1;i>=0;i--)
-        {
-            int profit = 0;
-            for(int j = 0;j<2;j++)
-            {
-                for(int k = 1;k<3;k++)
-                {
-                    if(j)
-                        profit = max(-prices[i]+after[0][k],after[1][k]);
-                    else
-                        profit = max(prices[i]+after[1][k-1],after[0][k]);
-                    curr[j][k] = profit;
-                }
-                after = curr;
-            }
-        }
-        
-        return after[1][2];
+        //memoization
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,-1)));
+        return f(0,1,prices,2,dp);
     }
 };
