3acbaa5fb5e584a64a9da016cb167a2de6d26b38
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-09 17:44:58 +0530

    Time: 3 ms (37.31%), Space: 6.7 MB (5.16%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -5,14 +5,16 @@ public:
     {
         if(wt<1 or n==0)
             return 1;
+        if(dp[wt][n]!=-1)
+            return dp[wt][n];
         if(wt<=n)
-            return max(wt*f(wt,n-wt,dp),f(wt-1,n,dp));
+            return dp[wt][n] = max(wt*f(wt,n-wt,dp),f(wt-1,n,dp));
         else
-            return f(wt-1,n,dp);
+            return dp[wt][n] = f(wt-1,n,dp);
     }
     
     int integerBreak(int n) {
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         if(n%2)
             return f((n/2)+1,n,dp);
         return f(n/2,n,dp);
