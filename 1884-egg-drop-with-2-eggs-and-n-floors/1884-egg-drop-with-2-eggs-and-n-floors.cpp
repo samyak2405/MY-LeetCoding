f3e4f6ff5efafca6ea91829b26337ad92bc75d32
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-11 21:39:46 +0530

    Time: 195 ms (16.89%), Space: 9.1 MB (9.22%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
-    int twoEggDrop(int n) {
-        int sum = 0;
-        for(int i = 1;i<1001;i++)
+    
+    int solve(int f,int e,vector<vector<int>> &dp)
+    {
+        if(e==0)
+            return 0;
+        if(e==1)
+            return f;
+        if(f==1 or f==0)
+            return f;
+        if(dp[f][e]!=-1)
+            return dp[f][e];
+        int mn = INT_MAX;
+        for(int k = 1;k<f;k++)
         {
-            sum+=i;
-            if(sum>=n)
-                return i;
+            int tmp = max(solve(k-1,e-1,dp),solve(f-k,e,dp));
+            mn = min(mn,tmp);
         }
-        return -1;
+        return dp[f][e] = mn+1;
+    }
+    
+    int twoEggDrop(int n) {
+        vector<vector<int>> dp(n+1,vector<int>(3,-1));
+        return solve(n,2,dp);
     }
 };
