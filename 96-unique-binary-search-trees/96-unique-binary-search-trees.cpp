d8c3370e987ea097ceb008b434073f1dbc488b42
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-12 11:38:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.51%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int numTrees(int n) {
-if(n==0 or n==1)
-            return 1;
-        
-        vector<int> v1(n+1,0);
-        v1[0]=1,v1[1]=1;
-        
-        for(int i = 2;i<=n;i++){
-            for(int j = 1;j<=i;j++)
-                v1[i] +=v1[j-1]*v1[i-j];
+        if(n==0 or n==1)
+            return n;
+        vector<int> dp(n+1,0);
+        dp[0] = dp[1]=1;
+        for(int i = 2;i<=n;i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
         }
-        for(auto it:v1)
-            cout<<it<<" ";
-        return v1[n];
+        return dp[n];
     }
 };
