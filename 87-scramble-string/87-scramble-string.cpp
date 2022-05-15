b1c1ae2e8b35474bae72e91f0be5ff05d913218f
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-15 12:06:39 +0530

    Time: 246 ms (28.41%), Space: 37.4 MB (19.58%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -9,15 +9,15 @@ public:
         if(mp.find(key)!=mp.end())
             return mp[key];
         int n = s1.length();
-        vector<int> cnt1(26,0);
-        for(int i = 0;i<n;i++)
-        {
-            cnt1[s1[i]-'a']++;
-            cnt1[s2[i]-'a']--;
-        }
-        for(int i = 0;i<26;i++)
-            if(cnt1[i]!=0)
-                return mp[key] = false;
+//         vector<int> cnt1(26,0);
+//         for(int i = 0;i<n;i++)
+//         {
+//             cnt1[s1[i]-'a']++;
+//             cnt1[s2[i]-'a']--;
+//         }
+//         for(int i = 0;i<26;i++)
+//             if(cnt1[i]!=0)
+//                 return mp[key] = false;
         
         for(int i = 1;i<=n-1;i++)
         {
