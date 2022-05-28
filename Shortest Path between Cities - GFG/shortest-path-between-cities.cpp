1e45be6970b13d5dba182585120c5d9696d2f216
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-05-28 11:55:21 +0530

    Added solution - LeetHub

--- Shortest Path between Cities - GFG/shortest-path-between-cities.cpp
+++ Shortest Path between Cities - GFG/shortest-path-between-cities.cpp
@@ -9,49 +9,19 @@ using namespace std;
 
 class Solution{   
 public:
-
-    void lca(int x,vector<int> &lcax)
-    {
-        while(x>1)
-        {
-            if(x%2==0)
-            {
-                lcax.emplace_back(x/2);
-                x/=2;
-            }
-            else
-            {
-                lcax.emplace_back((x-1)/2);
-                x=(x-1)/2;
-            }
-        }
-        // lcax.emplace_back(1);
-    }
-
     int shortestPath( int x, int y){ 
-        vector<int> lcax,lcay;
-        lcax.emplace_back(x);
-        lcay.emplace_back(y);
-        lca(x,lcax);
-        lca(y,lcay);
-        map<int,int> mp;
-        int ans = 0;
-        // for(int i = 0;i<lcax.size();i++)
-        //     cout<<lcax[i]<<" ";
-        // cout<<endl;
-        // for(int i = 0;i<lcay.size();i++)
-        //     cout<<lcay[i]<<" ";
-        for(int i = 0;i<lcax.size();i++)
-            mp[lcax[i]] = i;
-        for( int i = 0;i<lcay.size();i++)
-            if(mp.find(lcay[i])!=mp.end())
-            {
-                // cout<<mp[lcay[i]]<<" "<<i<<" ";
-                ans = mp[lcay[i]]+i;
-                break;
-            }
+        int cnt = 0;
         
-        return ans;
+        while(x!=y)
+        {
+            if(x>y)
+                x/=2;
+            else
+                y/=2;
+            cnt++;
+        }
+        
+        return cnt;
     }
 };
 
