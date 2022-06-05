1c5afbe04ecc78b7ab1e1afdfb1b2c263e8909de
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-05 10:08:58 +0530

    Time: 15 ms (29.76%), Space: 6.2 MB (46.51%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     
-    bool isSafe(int row,int col,vector<string> &board,int n)
+    bool isSafe(int row,int col,int n,vector<string> &board)
     {
         int duprow = row;
         int dupcol = col;
-        while(row<n and col>=0)
+        while(row>=0 and col>=0)
         {
             if(board[row][col]=='Q')
                 return false;
-            row++;
+            row--;
             col--;
         }
         row = duprow;
@@ -20,43 +20,40 @@ public:
                 return false;
             col--;
         }
-        row = duprow;
         col = dupcol;
-        while(row>=0 and col>=0)
+        while(row<n and col>=0)
         {
             if(board[row][col]=='Q')
                 return false;
-            row--;
+            row++;
             col--;
         }
         return true;
     }
     
-    void solve(int col,vector<string> &board,int &cnt,int n)
+    void solve(int col,int n,int &cnt,vector<string> &board)
     {
-        if(n==col)
+        if(col==n)
         {
             cnt++;
             return;
         }
         for(int row = 0;row<n;row++)
         {
-            if(isSafe(row,col,board,n))
+            if(isSafe(row,col,n,board))
             {
-                board[row][col] ='Q';
-                solve(col+1,board,cnt,n);
-                board[row][col] ='.';
+                board[row][col] = 'Q';
+                solve(col+1,n,cnt,board);
+                board[row][col] = '.';
             }
         }
     }
     
     int totalNQueens(int n) {
-        int cnt = 0;
-        vector<string> board(n);
         string s(n,'.');
-        for(int i = 0;i<n;i++)
-            board[i] = s;
-        solve(0,board,cnt,n);
+        vector<string> board(n,s);
+        int cnt = 0;
+        solve(0,n,cnt,board);
         return cnt;
     }
 };
