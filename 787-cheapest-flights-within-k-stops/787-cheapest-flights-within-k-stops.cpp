5386572cfc53bc59b0209bd09ff3c15f0c25d8bb
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-09 11:29:28 +0530

    Time: 275 ms (10.68%), Space: 49.5 MB (14.37%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,20 +1,76 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>> dp(k+2,vector<int>(n,INT_MAX));
-        for(int i = 0;i<=k+1;i++)
-            dp[i][src] = 0;
-        for(int i = 1;i<=k+1;i++)
+        vector<int> dist(n,INT_MAX);
+        dist[src] = 0;
+        
+        for(int i = 0;i<=k;i++)
         {
+            vector<int> tmp(dist);
+            
             for(auto it:flights)
             {
-                int u = it[0];
-                int v = it[1];
-                int w = it[2];
-                if(dp[i-1][u]!=INT_MAX)
-                    dp[i][v] = min(dp[i][v],dp[i-1][u]+w);
+                if(dist[it[0]]!=INT_MAX)
+                    tmp[it[1]] = min(tmp[it[1]],dist[it[0]]+it[2]);
             }
+            dist = tmp;
         }
-        return (dp[k+1][dst]==INT_MAX)?-1:dp[k+1][dst];
+        return dist[dst] == INT_MAX?-1:dist[dst];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        //Using BFS:TLE
+//         unordered_map<int,vector<pair<int,int>>> adj;
+//         for(auto it:flights)
+//             adj[it[0]].push_back({it[1],it[2]});
+//         queue<pair<int,int>> q;
+//         q.push({src,0});
+//         int stod = INT_MAX;
+        
+//         while(!q.empty() and k-->=0)
+//         {
+//             int sz = q.size();
+//             for(int i = 0;i<sz;i++)
+//             {
+//                 int u = q.front().first;
+//                 int w = q.front().second;
+//                 q.pop();
+//                 for(auto it:adj[u])
+//                 {
+//                     if(stod<w+it.second)
+//                         continue;
+//                     q.push({it.first,w+it.second});
+//                     if(dst==it.first)
+//                         stod = min(stod,w+it.second);
+//                 }
+//             }
+//         }
+//         return stod==INT_MAX?-1:stod;
     }
 };
