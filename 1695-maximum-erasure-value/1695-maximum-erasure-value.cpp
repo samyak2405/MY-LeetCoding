1af25570976f66955d070bcb307b1ec3134b67e2
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-12 10:11:17 +0530

    Time: 649 ms (28.19%), Space: 127.3 MB (24.42%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,7 +4,8 @@ public:
         int res = 0;
         unordered_set<int> s1;
         int n = nums.size();
-        for(int i = 0,j=0,win=0;j<n;j++)
+        int win=0,i = 0;
+        for(int j=0;j<n;j++)
         {
             while(s1.find(nums[j])!=s1.end())
             {
