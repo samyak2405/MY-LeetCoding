db5c6fec19da129a290461a40c8b6ba31a247be0
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-13 09:24:54 +0530

    Time: 7 ms (75.60%), Space: 9.1 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    
-//     int f(int i,int j,int n,vector<vector<int>> &triangle,vector<vector<int>> &dp)
-//     {
-//         if(i==n-1)
-//             return triangle[n-1][j];
-        
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
-        
-//         return dp[i][j] = triangle[i][j] + min(f(i+1,j,n,triangle,dp),f(i+1,j+1,n,triangle,dp));
-//     }
+    int f(int i,int j,vector<vector<int>> &triangle,vector<vector<int>> &dp)
+    {
+        if(i==triangle.size())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int v1 = f(i+1,j,triangle,dp);
+        int v2 = f(i+1,j+1,triangle,dp);
+        return dp[i][j] = triangle[i][j] + min(v1,v2);
+    }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        // vector<vector<int>> dp(n,vector<int>(n,-1));
-        // return f(0,0,n,triangle,dp);
-        // vector<vector<int>> dp(n,vector<int>(n,0));
-        vector<int> ahead(n,0),curr(n,0);
-        for(int i = 0;i<n;i++)
-            ahead[i] = triangle[n-1][i];
-        for(int i = n-2;i>=0;i--)
-        {
-            for(int j = i;j>=0;j--)
-                curr[j] = triangle[i][j] + min(ahead[j],ahead[j+1]);
-            ahead = curr;
-        }
-        return ahead[0];
+        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));
+        return f(0,0,triangle,dp);
     }
 };
