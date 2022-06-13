230387c95964b71a1cf40d757a2be088e44d7d9f
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-13 10:54:24 +0530

    Time: 17 ms (26.06%), Space: 8.1 MB (47.04%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        
+        for(int i = 0;i<moves.size();i++)
+        {
+            if(moves[i]=='U')
+                x+=1;
+            if(moves[i]=='D')
+                x-=1;
+            if(moves[i]=='L')
+                y-=1;
+            if(moves[i]=='R')
+                y+=1;
+        }
+        
+        return x==0 and y==0;
+    }
+};
