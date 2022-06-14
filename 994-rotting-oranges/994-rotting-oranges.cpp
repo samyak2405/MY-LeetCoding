3408ab94ca679d89bc4dc34f761e0c0183fa9389
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-06-14 15:28:07 +0530

    Time: 14 ms (35.53%), Space: 14.6 MB (5.81%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -13,7 +13,6 @@ public:
         bool flag = false;
         queue<pair<int,int>> q;
         vector<vector<int>> lvl(m,vector<int>(n,0));
-        vector<vector<int>> vis(m,vector<int>(n,0));
         for(int i = 0;i<m;i++)
         {
             for(int j = 0;j<n;j++)
@@ -21,7 +20,6 @@ public:
                 if(grid[i][j]==2)
                 {
                     q.push({i,j});
-                    vis[i][j] = 1;
                     lvl[i][j] = 0;
                 }
                 if(grid[i][j]==1)
@@ -45,13 +43,10 @@ public:
                 int c_y = it[1] + j;
                 if(!isValid(c_x,c_y,m,n))
                     continue;
-                if(vis[c_x][c_y])
-                    continue;
-                if(grid[c_x][c_y]==0)
+                if(grid[c_x][c_y]==0 or grid[c_x][c_y]==2)
                     continue;
                 q.push({c_x,c_y});
                 grid[c_x][c_y] = 2;
-                vis[c_x][c_y] = 1;
                 lvl[c_x][c_y] = lvl[i][j] + 1;
                 ans = max(ans,lvl[c_x][c_y]);
             }
