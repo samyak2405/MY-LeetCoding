b9181fe2d7b974d8f01a0d01be40ac4ab72fb8d3
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-06 18:06:47 +0530

    Time: 7 ms (27.16%), Space: 7.1 MB (32.37%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    
-    int f(int ind,int prev,int flag,vector<int> &nums,vector<vector<vector<int>>> &dp)
-    {
-        if(ind==nums.size())
-            return 0;
-        if(dp[ind][prev+1][flag]!=-1)
-            return dp[ind][prev+1][flag];
-        int nt = f(ind+1,prev,flag,nums,dp);
-        int t = 0;
-        if(prev==-1)
-            t = 1 + f(ind+1,ind,1-flag,nums,dp);
-        else
-        {
-            if(!flag and (nums[ind]>nums[prev]))
-                t = 1 + f(ind+1,ind,1-flag,nums,dp);
-            else if(flag and nums[ind]<nums[prev])
-                t = 1+f(ind+1,ind,1-flag,nums,dp);
-        }
-        return dp[ind][prev+1][flag] = max(t,nt);
-    }
-    
     int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
-        return max(f(0,-1,0,nums,dp),f(0,-1,1,nums,dp));
+        int up = 1;
+        int down = 1;
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[i-1])
+                up = down+1;
+            else if(nums[i]<nums[i-1])
+                down = up+1;
+        }
+        return max(up,down);
     }
 };
