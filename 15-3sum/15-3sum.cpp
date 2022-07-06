c826b4fec2812d474cc0ae8b3cfaf23d96efb12b
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-06 11:26:48 +0530

    Time: 152 ms (47.98%), Space: 21.2 MB (47.03%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -4,30 +4,36 @@ public:
         vector<vector<int>> res;
         if(nums.size()<3)
             return res;
-        set<vector<int>> s1;
         sort(nums.begin(),nums.end());
         int n = nums.size();
         for(int i = 0;i<n-2;i++)
         {
-            int j = i+1,k = n-1;
-            while(j<k)
+            if(i==0 or (i>0 and nums[i-1]!=nums[i]))
             {
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)
+                int j = i+1,k = n-1;
+                int sum = -nums[i];
+                while(j<k)
                 {
-                    vector<int> tmp = {nums[i],nums[j],nums[k]};
-                    s1.insert(tmp);
-                    j++;
-                    k--;
+                    int ans = nums[j]+nums[k];
+                    if(sum==ans)
+                    {
+                        vector<int> tmp = {nums[i],nums[j],nums[k]};
+                        res.emplace_back(tmp);
+                        while(j<k and nums[j]==nums[j+1])
+                            j++;
+                        while(j<k and nums[k]==nums[k-1])
+                            k--;
+                        j++;
+                        k--;
+                    }
+                    else if(sum>ans)
+                        j++;
+                    else
+                        k--;
                 }
-                else if(sum<0)
-                    j++;
-                else
-                    k--;
             }
+            
         }
-        for(auto it:s1)
-            res.push_back(it);
         return res;
     }
 };
