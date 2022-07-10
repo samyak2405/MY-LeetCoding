a6a7b6731dc83af9d0c2d1a333319397e60b928a
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-10 10:58:05 +0530

    Time: 192 ms (69.82%), Space: 108 MB (74.32%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -3,25 +3,53 @@ public:
     int numOfSubarrays(vector<int>& arr) {
         int mod = 1e9+7;
         int n = arr.size();
-        vector<int> even(n),odd(n);
+        // vector<int> even(n),odd(n);
+        // if(arr[n-1]%2)
+        //     odd[n-1] = 1;
+        // else
+        //     even[n-1] =1;
+        // int sum = odd[n-1];
+        // for(int i = n-2;i>=0;i--)
+        // {
+        //     if(arr[i]%2==0)
+        //     {
+        //         even[i] = (1+even[i+1])%mod;
+        //         odd[i] =odd[i+1];
+        //     }
+        //     else
+        //     {
+        //         odd[i] = (1+even[i+1])%mod;
+        //         even[i] = odd[i+1];
+        //     }
+        //     sum = (sum+odd[i])%mod;
+        // }
+        // return sum;
+        int even = 0,odd = 0,odd_next,even_next;
+        int sum = 0;
         if(arr[n-1]%2)
-            odd[n-1] = 1;
+            odd = 1;
         else
-            even[n-1] =1;
-        int sum = odd[n-1];
+            even = 1;
+        odd_next = odd;
+        even_next = even;
+        sum+=odd;
         for(int i = n-2;i>=0;i--)
         {
             if(arr[i]%2==0)
             {
-                even[i] = (1+even[i+1])%mod;
-                odd[i] =odd[i+1];
+                even = (1+even_next)%mod;
+                odd = odd_next;
+                even_next = even;
+                odd_next = odd;
             }
             else
             {
-                odd[i] = (1+even[i+1])%mod;
-                even[i] = odd[i+1];
+                odd = (1+even_next)%mod;
+                even = odd_next;
+                odd_next = odd;
+                even_next = even;
             }
-            sum = (sum+odd[i])%mod;
+            sum = (sum + odd)%mod;
         }
         return sum;
     }
