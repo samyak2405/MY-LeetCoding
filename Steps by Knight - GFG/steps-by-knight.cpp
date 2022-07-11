1d56176fba2f192dc7d2f66334edda054f2f4e39
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-11 16:45:08 +0530

    Added solution - LeetHub

---
+++ Steps by Knight - GFG/steps-by-knight.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find out minimum steps Knight needs to reach target position.\
+    
+    bool isValid(int i,int j,int n)
+    {
+        return i>=0 and j>=0 and i<n and j<n;
+    }
+    
+	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int N)
+	{
+	    int res = 0;
+	    queue<pair<int,int>> q;
+	    vector<vector<int>> grid(N+1,vector<int>(N+1));
+	    
+	    q.push({KnightPos[0],KnightPos[1]});
+	    grid[KnightPos[0]][KnightPos[1]] =1;
+	    
+	    int dx[8] = {-2,-2,1,-1,2,2,-1,1};
+	    int dy[8] = {-1,1,-2,-2,-1,1,2,2};
+	    while(!q.empty())
+	    {
+	        int sz = q.size();
+	        for(int i = 0;i<sz;i++)
+	        {
+	            int x1 = q.front().first;
+	            int y1 = q.front().second;
+	            q.pop();
+	            if(x1==TargetPos[0] and y1==TargetPos[1])
+	                return res;
+	            for(int k = 0;k<8;k++)
+	            {
+	                int c_x1 = x1+dx[k];
+	                int c_y1 = y1+dy[k];
+	                if(!isValid(c_x1,c_y1,N+1) or grid[c_x1][c_y1])
+	                    continue;
+	               grid[c_x1][c_y1] = 1;
+	               q.push({c_x1,c_y1});
+	            }
+	        }
+	        res++;
+	    }
+	    return res;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		vector<int>KnightPos(2);
+		vector<int>TargetPos(2);
+		int N;
+		cin >> N;
+		cin >> KnightPos[0] >> KnightPos[1];
+		cin >> TargetPos[0] >> TargetPos[1];
+		Solution obj;
+		int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
