7d2a04965bdf81be9352b62810a3774b7e7988f0
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-12 14:51:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.10%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int> arr;
-        for(int i = 0;i<n;i++)
-            arr.push_back(2*i+1);
-        int median = 0;
-        if(n%2)
-            median = arr[n/2];
-        else
-            median = arr[n/2]-1;
-        int sum = 0;
-        for(int i = 0;i<n/2;i++)
-            sum+=abs(arr[i]-median);
-        return sum;
+        return n*n/4;
     }
 };
