daf60ee2a235c91a6e21d95c86b9fc17a7d80a71
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-15 15:27:45 +0530

    Added solution - LeetHub

---
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+double MedianOfArrays(vector<int>& array1, vector<int>& array2);
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+    {
+        vector<int> res;
+        for(auto it:array1)
+            res.push_back(it);
+        for(auto it:array2)
+            res.push_back(it);
+        sort(res.begin(),res.end());
+        int n = res.size();
+        if(res.size()%2)
+            return (double)res[n/2];
+        return (double)(res[n/2]+res[(n/2)-1])/2.0;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        cin>>n;
+        vector<int> array2(n);
+        for (int i = 0; i < n; ++i){
+            cin>>array2[i];
+        }
+        Solution ob;
+        cout<<ob.MedianOfArrays(array1, array2)<<endl;
+    }
+    return 0; 
+}
+
+
+  // } Driver Code Ends
