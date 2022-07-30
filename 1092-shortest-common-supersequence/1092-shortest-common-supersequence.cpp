1fa7a6563db44030388ecf75d6f26011b405cae5
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-07-30 21:37:06 +0530

    Time: 27 ms (60.73%), Space: 12.8 MB (53.24%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     string shortestCommonSupersequence(string s1, string s2) {
         int n = s1.length(),m = s2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int i = 0;i<n+1;i++)
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i = 1;i<n+1;i++)
         {
-            for(int j = 0;j<m+1;j++)
+            for(int j = 1;j<m+1;j++)
             {
-                if(i==0 or j==0)
-                    dp[i][j] = 0;
-                else if(s1[i-1]==s2[j-1])
+                if(s1[i-1]==s2[j-1])
                     dp[i][j] = 1+dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
