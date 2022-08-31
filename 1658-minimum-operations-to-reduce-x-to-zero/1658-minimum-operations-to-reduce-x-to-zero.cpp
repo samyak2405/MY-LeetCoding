58484a7d1a4dabf6cd7087e070a6c615eb5936c4
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-08-31 13:52:30 +0530

    Time: 538 ms (30.01%), Space: 153.5 MB (24.74%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,21 +4,20 @@ public:
         int target = -x;
         for(auto it:nums)
             target+=it;
+        int n = nums.size();
         if(target==0)
-            return nums.size();
+            return n;
         int res = INT_MIN;
+        int sum = 0;
         unordered_map<int,int> mp;
         mp[0] = -1;
-        int sum = 0;
-        for(int i = 0;i<nums.size();i++)
+        for(int i = 0;i<n;i++)
         {
             sum+=nums[i];
             if(mp.find(sum-target)!=mp.end())
-            {
                 res = max(res,i-mp[sum-target]);
-            }
             mp[sum] = i;
         }
-        return res==INT_MIN?-1:nums.size()-res;
+        return res == INT_MIN?-1:n-res;
     }
 };
