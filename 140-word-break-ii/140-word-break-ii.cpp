031e5c28dcb1b9e64c87b3c118a0c60e96d23c79
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-09-22 16:49:03 +0530

    Time: 9 ms (6.70%), Space: 7.1 MB (83.12%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void f(int ind,string s,unordered_set<string> &s1,string tmp,vector<string> &res)
+    void f(string s,int ind,string tmp,unordered_set<string> &s1,vector<string> &res)
     {
         if(ind==s.length())
         {
@@ -9,19 +9,21 @@ public:
             res.push_back(tmp);
             return;
         }
-        string store;
-        for(int k = ind;k<s.length();k++)
+        string store = "";
+        for(int i = ind;i<s.length();i++)
         {
-            store += s[k];
+            store += s[i];
             if(s1.find(store)!=s1.end())
-                f(k+1,s,s1,tmp+store+" ",res);
+                f(s,i+1,tmp+store+" ",s1,res);
         }
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<string> res;
         unordered_set<string> s1(wordDict.begin(),wordDict.end());
-        f(0,s,s1,"",res);
+        vector<string> res;
+        f(s,0,"",s1,res);
         return res;
-    }
+    } 
+    
+   
 };
