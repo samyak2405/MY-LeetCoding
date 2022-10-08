20cecd90f0746d99eb1b16489a6c8bca58e2a5f6
Samyak moon <38495022+samyak2405@users.noreply.github.com>
2022-10-08 09:06:33 +0530

    Time: 452 ms (16.68%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-update Salary set sex = CHAR(ASCII('m')^ASCII('f')^ASCII(sex));
+# update Salary set sex = CHAR(ASCII('m')^ASCII('f')^ASCII(sex));
+update Salary set sex = if(sex = 'm','f','m');
